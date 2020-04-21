c06da9cd72c76d1ea02b8388cbe043ec2a40fd61
Danish864 <63333356+Danish864@users.noreply.github.com>
2020-04-21 15:51:05 +0500

    Update Assignment1st.py

--- Assignment1st.py
+++ Assignment1st.py
@@ -1,6 +1,6 @@
 a = input("Enter First Number !")
 b = input("Enter second Number !")
-sum = float(a)+float(b)
+sum = float(a)+float(b) # you can write int too
 subtraction = int(a)-int(b)
 multiplication = int(a)*int(b)
 division = int(a)/int(b)
